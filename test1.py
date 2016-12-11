46804055a0e04532ac45ed4c0e9f94cfeda811a9
amoursu <allenzhou15@163.com>
2016-12-12 00:20:40 +0800

    test:Don't hide methods via attributes

--- test1.py
+++ test1.py
@@ -10,3 +10,23 @@ def cmp(x, y):
 	print("{0} ^ {1}".format(x, y))
 
 pow(2,3)
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.area = width * height
+
+    # hidden because of attribute with same name
+    def area(self, value=None):
+        if value == None:
+            return self.area
+        else:
+            self.area = value
+
+
+r = Rectangle(3, 4)
+
+    # TypeError: int is not callable
+print(r.area())
