9dd07e7ea5dbea7b0b26d48365210f34d3698169
amoursu <allenzhou15@163.com>
2016-12-11 21:29:23 +0800

    test: Check overload of built-in function or attribute

--- test1.py
+++ test1.py
@@ -1,3 +1,12 @@
 a1, a2, b1, b2 = 1, 2, 3, 4
 if a1 < a2 == b1 < b2:
-  print "The condition evaluated to True"
+	print "The condition evaluated to True"
+
+
+def pow(x, y):
+	print("{0} ^ {1}".format(x, y))
+
+def cmp(x, y):
+	print("{0} ^ {1}".format(x, y))
+
+pow(2,3)
---
+++ test2.py
@@ -0,0 +1,2 @@
+def cmp(x, y, z):
+	print("{0} ^ {1}".format(x, y))
